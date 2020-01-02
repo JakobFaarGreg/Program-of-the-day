dc74b1c9a852aecc369cac35370548416f794db5
Jakob F Gregersen <lilleper.jfg@gmail.com>
2020-01-02 16:09:44 +0100

    create dag2.c
    Program for calculating length of fishing line from volume on barrel

---
+++ dag2.c
@@ -0,0 +1,65 @@
+/* Udregning af volume udfra tykkelse og laengde */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define METER_TO_CENTIMETER 100
+#define MILLIMETER_TO_CENTIMETER 10
+#define CENTIMETER_TO_METER 100
+
+double prompt_for_diameter();
+int    prompt_for_length();
+double calculate_volume_on_barrel(double, int);
+void   print_volume_to_length(double volume);
+double calculate_length_from_volume(double diameter, double volume);
+
+int main() {
+    int lineLength = 0;
+    double lineDiameter = 0, volume = 0;
+    
+    lineDiameter = prompt_for_diameter();
+    lineLength   = prompt_for_length();
+    volume = calculate_volume_on_barrel(lineDiameter, lineLength);
+    printf("Der er %f kubikcentimeter plads paa tromlen. \n", volume);
+
+    printf("Baseret på den volume kan der vaere snoerer i disse tykkelser i disse laengder:\n");
+    print_volume_to_length(volume);
+    return EXIT_SUCCESS;
+}
+
+double prompt_for_diameter() {
+    double diameter = 0;
+    printf("Hvilken bredde fiskesnoere, i mm, oensker du? \n");
+    scanf(" %lf", &diameter);
+    return diameter;
+}
+
+int prompt_for_length() {
+    int length = 0;
+    printf("Hvilken laengde fiskesnoere, i meter, oensker du? \n");
+    scanf(" %d", &length);
+    return length;
+}
+
+double calculate_volume_on_barrel(double diameter, int length) {
+    double volume = 0;
+    diameter = diameter / MILLIMETER_TO_CENTIMETER;
+    length = length * METER_TO_CENTIMETER;
+    volume = ((pow((diameter / 2), 2) * M_PI) * length);
+    return volume;
+}
+
+/* Skal printe en tabel med nogle forskellige snoeretykkelser og deres laengeste laengde */
+void print_volume_to_length(double volume){
+    int i = 0;
+    for (i = 0; i < 10; i++) {
+        printf("%.2lf mm. tykkelse....%.2lf meter laengde\n", (0.28 + i/100), calculate_length_from_volume((0.28 + i/100), volume));
+    }
+}
+
+/* Skal udregne laengde af forskellige tykkelse snoerer */
+double calculate_length_from_volume(double diameter, double volume){
+    double length = 0;
+    length = (volume / (pow(diameter/2, 2) * M_PI));
+    length *= CENTIMETER_TO_METER;
+    return length;
+}
