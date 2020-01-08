451e6f7e8f5a3f01260ee86adad62ded21d33c55
Jakob F Gregersen <lilleper.jfg@gmail.com>
2020-01-08 11:33:54 +0100

    Update dag8.c

--- dag8.c
+++ dag8.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #define NAME_MAX_LENGTH 10
 
+int cmpfunc (const void * a, const void * b);
+
 struct person {
     int age;
     char name[NAME_MAX_LENGTH];
@@ -19,5 +21,12 @@ int main() {
     printf(" %d - %s\n", p3.age, p3.name);
     printf(" %d - %s\n", p4.age, p4.name);
 
+    for (i = 0; i < 4; i++) {
+        printf(" %d - %s\n", p1.age, p1.name);
+    }
+
     return EXIT_SUCCESS;
+}
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
 }
