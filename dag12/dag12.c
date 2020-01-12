2f80ba8c1ba22a2719a1ae81423da270184fc8b6
Jakob F Gregersen <lilleper.jfg@gmail.com>
2020-01-12 14:34:07 +0100

    dag12 created

---
+++ dag12/dag12.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+
+
+int main() {
+    FILE *dag12_ptr;
+    int i = 0;
+    
+    dag12_ptr = fopen("dag12_ptr", "r+");
+    if (dag12_ptr != NULL) {
+        fscanf(dag23_ptr, )
+    }
+
+    return EXIT_SUCCES;
+}
+
