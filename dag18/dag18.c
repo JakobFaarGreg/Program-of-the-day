6a877a3424fa905f1eaf9adc00103d28a13ee67c
Jakob F Gregersen <lilleper.jfg@gmail.com>
2020-01-19 14:30:50 +0100

    Update dag18.c
    Trinvis forfinelse i programmet

--- dag18/dag18.c
+++ dag18/dag18.c
@@ -14,24 +14,17 @@ wing array[NUMBER_OF_WINGS];
 
 char* prompt_for_name();
 void print_name_list();
+void print_wing_information(char* userInput);
+void load_wing_array_from_file(FILE* file);
 
 int main() {
     FILE *file_pointer;
-    int i = 0;
-    char *userInput, ch = '0';
+    char *userInput;
 
     /* Her laver jeg en filpointer der peger på filen wing.txt og er i read tilstand. */
     file_pointer = fopen("wing.txt", "r");
-    if (file_pointer != NULL){
-        /* Her er en do-while løkke der ignorerer den første linje i dokumentet wing.txt. */
-        do {
-            ch = fgetc(file_pointer);
-        } while (ch != '.');
-        /* Her indlæses alle vingerne i deres structs. */
-        for (i = 0; i < NUMBER_OF_WINGS; i++){
-            fscanf(file_pointer, " %s - %d - %d - %d", array[i].name, &array[i].size, &array[i].productionCost, &array[i].salesPrice);
-        }
-    }
+    load_wing_array_from_file(file_pointer);
+
     /* Her printer jeg en liste over de vinger jeg har i tekstdokumentet. */
     print_name_list();
 
@@ -39,16 +32,28 @@ int main() {
     userInput = prompt_for_name();
 
     /* Her printer jeg kun den ene vinge, hvis navn matcher tekststrengen fra brugeren. */
-    for (i = 0; i < NUMBER_OF_WINGS; i++){
-        if (strcmp(array[i].name, userInput) == 0){
-            printf("The wing you wanted to know about, %s,\n", userInput);
-            printf("Is %d meters long, it costs %d DKK to make and is sold at %d DKK\n", array[i].size, array[i].productionCost, array[i].salesPrice);
-        }
-    }
+    print_wing_information(userInput);
+
     /* Her lukker jeg filpointeren. */
     fclose(file_pointer);
     return EXIT_SUCCESS;
 }
+
+void load_wing_array_from_file(FILE* file){
+    int i = 0;
+    char ch = '0';
+    if (file != NULL){
+        /* Her er en do-while løkke der ignorerer den første linje i dokumentet wing.txt. */
+        do {
+            ch = fgetc(file);
+        } while (ch != '.');
+        /* Her indlæses alle vingerne i deres structs. */
+        for (i = 0; i < NUMBER_OF_WINGS; i++){
+            fscanf(file, " %s - %d - %d - %d", array[i].name, &array[i].size, &array[i].productionCost, &array[i].salesPrice);
+        }
+    }
+}
+
 
 void print_name_list(){
     int i = 0;
@@ -64,4 +69,21 @@ char* prompt_for_name(){
     printf("Type out the wing you want to know more about: \n");
     scanf(" %s", name);
     return name;
+}
+
+void print_wing_information(char* userInput){
+    int i = 0, notFound = 0;
+    for (i = 0; i < NUMBER_OF_WINGS; i++){
+        if (strcmp(array[i].name, userInput) == 0){
+            printf("The wing you wanted to know about, %s,\n", userInput);
+            printf("Is %d meters long, it costs %d DKK to make and is sold at %d DKK\n", array[i].size, array[i].productionCost, array[i].salesPrice);
+        }
+        else {
+            notFound++;
+        }
+    }
+    if (notFound == i){
+        printf("Invalid name. Terminating program!\n");
+        exit(EXIT_FAILURE);
+    }
 }
